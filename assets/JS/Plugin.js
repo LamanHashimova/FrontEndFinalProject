f5ae87dd8ec8dc6a90d8338b9db67427a99783ae
LamanHashimova <112489993+LamanHashimova@users.noreply.github.com>
2023-04-19 21:28:33 +0400

    commit

---
+++ assets/JS/Plugin.js
@@ -0,0 +1,127 @@
+
+
+
+$('.sliderBody').slick({
+    dots: false,
+    arrows:false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+  margin: 10,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 4
+    }
+  }
+})
+
+$('.word-all').slick({
+    dots: false,
+    arrows:false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+// var owl = $('.word-all');
+// owl.owlCarousel({
+//   margin: 10,
+//   loop: true,
+//   responsive: {
+//     0: {
+//       items: 1
+//     },
+//     600: {
+//       items: 2
+//     },
+//     1000: {
+//       items: 4
+//     }
+//   }
+// })
+
+
+$(function () {
+    
+      var austDay =new Date();
+ 
+      austDay =new Date(austDay.getFullYear() + 1, 1 - 1, 26);
+
+      $('#defaultCountdown').countdown({until: austDay});
+  
+      $('#year').text(austDay.getFullYear());
+
+    });
+    
